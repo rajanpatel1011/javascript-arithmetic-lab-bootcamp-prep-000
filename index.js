7e536f1cdc1cc8024dddb693e18201ba73dffbcb
Ryan Patel <rajanpatel1011@gmail.com>
2018-03-13 15:41:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a,b){
+  return a+b;
+}
+function subtract(a,b){
+  return a-b;
+}
+function multiply(a,b){
+  return a*b;
+}
+function divide(a,b){
+  return a/b;
+}
+function inc(n){
+  return n+1;
+}
+function dec(n){
+  return n-1;
+}
+function makeInt(n){
+  return parseInt(n);
+}
+function makeInt(n,b){
+  b=10;
+  return parseInt(n,b);
+}
+function preserveDecimal(n){
+  n=2.222;
+  return parseFloat(n);
+}
+function preserveDecimal(n){
+  n='sldkjflksjf';
+  return parseFloat(n);
+}
